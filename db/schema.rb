a460ca82290d232206a37163362523fa9900962d
Thomas <soler.thomas.06@gmail.com>
2020-04-15 14:04:39 +0200

    implemented playback functionality on show audio_message page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_112056) do
+ActiveRecord::Schema.define(version: 2020_04_15_113447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_112056) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
     t.index ["user_id"], name: "index_audio_messages_on_user_id"
   end
 
