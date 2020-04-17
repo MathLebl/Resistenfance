f4016803451f2d5c6e5e2cfa34dd18856f78864b
David <davidobob@laposte.net>
2020-04-17 16:03:25 +0200

    added message crypter / decryter function

--- app/services/message_crypter.rb
+++ app/services/message_crypter.rb
@@ -1,1 +1,29 @@
-message_crypter.rb
+class MessageCrypter
+  def initialize(message)
+    @message = message
+    @shift = 2
+  end
+
+  def encrypt
+    alphabet = ("a".."z").to_a
+    letters = @message.split("")
+    crypted_message = ""
+    letters.each do |letter|
+      is_letter_upcased = letter == letter.upcase
+      letter = letter.downcase
+      if alphabet.include?(letter)
+        letter_index = alphabet.index(letter)
+        crypted_letter = alphabet[(letter_index - @shift)]
+        crypted_message += is_letter_upcased ? crypted_letter.upcase : crypted_letter
+      else
+        crypted_message += letter
+      end
+    end
+    crypted_message
+  end
+
+  def decrypt
+    encrypt(@message, -@shift)
+  end
+
+end
