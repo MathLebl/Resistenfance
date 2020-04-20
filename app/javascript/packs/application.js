6987f49b039021dcd490e61cbf280403524623e1
David <davidobob@laposte.net>
2020-04-20 13:40:14 +0200

    undommented record audio import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@ require("channels")
 import "bootstrap";
 
 import { eventListener } from './message_creator.js'
-// import { recordAudio } from './sound_recorder.js'
+import { recordAudio } from './sound_recorder.js'
 
 if (window.location.pathname.endsWith('/audio_messages')) {
   console.log('bonjour');
