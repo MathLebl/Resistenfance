fe0ebd8a0ee7927d501a92e4bcd38c1d73b604f6
amineneghbel <amineneghbel@gmail.com>
2020-04-24 12:32:17 +0200

    Seed avec 2 users et 2 messages, grade en forme de liste (8 grades dispo), description supprimée de audiomessage.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+p "Cleaning DataBase"
+User.destroy_all
 p '------------------'
 p 'Starting seeds ...'
-User.create!({pseudo: 'Chuck Norris', grade: 'sergent', email: 'test@test.com', password: 'testing123'})
+user1 = User.create!({pseudo: 'Fox Trot', grade: 'Sergent', email: 'foxtrot@test.com', password: 'testing123'})
+
+user2 = User.create!({pseudo: 'Chuck Norris', grade: 'Lieutenant', email: 'chucknorris@test.com', password: 'testing123'})
+'------------------'
+
+audios_attributes = [
+  {
+    user: user1,
+    audio_file: 'user1audio.mp3'
+  },
+  {
+    user: user2,
+    audio_file: 'user2audio.mp3'
+  },
+]
+
+audios_attributes.each do |audios_attribute|
+  audio_file = audios_attribute.delete(:audio_file)
+  audio = AudioMessage.create!(audios_attribute)
+  audio.audio_file.attach(io: File.open("audios/#{audio_file}"), filename: 'toto.mp3')
+end
+
+p '---------------'
+
 p 'Finished !'
-p '------------------'
+
