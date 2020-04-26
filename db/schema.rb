3d29c97add7c02c0361a4f1ee24bc2a446b4730d
David <davidobob@laposte.net>
2020-04-26 14:36:31 +0200

    added likes column to audio messages table, with default value as 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_100649) do
+ActiveRecord::Schema.define(version: 2020_04_26_123348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_04_24_100649) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "likes", default: 0
     t.index ["user_id"], name: "index_audio_messages_on_user_id"
   end
 
