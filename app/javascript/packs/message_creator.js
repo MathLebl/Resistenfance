3daec0cd5404f6075db00217d908d7f0a9cb0fc5
David <davidobob@laposte.net>
2020-05-01 16:02:56 +0200

    fixed selection of the correct comment form in audio message index

--- app/javascript/packs/message_creator.js
+++ app/javascript/packs/message_creator.js
@@ -1,31 +1,37 @@
 const eventListener = () => {
-    const form = document.querySelector('#search-form')
-        // console.log(form)
-    form.addEventListener('submit', (e) => {
+  document.addEventListener('click', (e) => {
+    // console.log(e.target.parentNode.dataset.audioMessageId);
+    if (e.target.className === "form") {
+      const form = e.target.parentNode
+      // console.log(form)
+      form.addEventListener('submit', (e) => {
         e.preventDefault();
-        messageGetter();
-    })
+        // console.log(e.target.dataset.audioMessageId);
+        messageGetter(form, e.target.dataset.audioMessageId);
+      })
+    }
+  })
 }
 
-const messageGetter = () => {
-    const form = document.querySelector('#form')
-    const message = form.value
-    postMessage(message);
-    form.value = "";
+const messageGetter = (parentForm, audioMessageId) => {
+  const form = parentForm.childNodes[1];
+  const message = form.value
+  postMessage(message, audioMessageId);
+  form.value = "";
 }
 
-const postMessage = (message) => {
-    fetch('/comments', {
-            method: 'POST',
-            headers: {
-                'Content-Type': "application/json"
-            },
-            body: JSON.stringify({ audio_message_id: 1, message: message })
-        })
-        .then(response => response.json())
-        .then((data) => {
-            // console.log(data.comment);
-        })
+const postMessage = (message, audioMessageId) => {
+  fetch('/comments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': "application/json"
+    },
+    body: JSON.stringify({ audio_message_id: audioMessageId, message: message })
+  })
+  .then(response => response.json())
+  .then((data) => {
+      // console.log(data.comment);
+  })
 }
 
 eventListener();
