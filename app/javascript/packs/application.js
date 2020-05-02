f4d0fc399175bc95fe9c89af19f0fe6811db1c44
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-05-02 20:47:17 +0200

    mecanique de toggle set

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,15 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+// Add toggle effet to Comments
+const btn = document.querySelector('buton')
+
+const commentsBlock = document.querySelector('.audio-message-comments')
+
+let isVisible = false
+
+btn.addEventListener('click', () => {
+  isVisible = !isVisible;
+  isVisible ? commentsBlock.classList.add('is-visible') : commentsBlock.classList.remove('is-visible');
+});
